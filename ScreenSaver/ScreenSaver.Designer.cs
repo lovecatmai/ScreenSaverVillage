c3cbb06858ccd6f5e5e672f32d8869955f5206af
Maksim <hirataakio875@gmail.com>
2026-02-05 20:07:13 +0300

    Added background and snowflake imgs, backImg painted

---
+++ ScreenSaver/ScreenSaver.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace ScreenSaver
+{
+    partial class ScreenSaver
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // ScreenSaver
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "ScreenSaver";
+            Text = "ScreenSaver";
+            WindowState = FormWindowState.Maximized;
+            Paint += ScreenSaver_Paint;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ ScreenSaver/ScreenSaver.cs
@@ -0,0 +1,22 @@
+using ScreenSaver.Properties;
+
+namespace ScreenSaver
+{
+    public partial class ScreenSaver : Form
+    {
+        private Bitmap backgroundImg;
+        private Bitmap snowflakeImg;
+
+        public ScreenSaver()
+        {
+            InitializeComponent();
+            backgroundImg = Resources.BackgroundImg;
+            snowflakeImg = Resources.SnowflakeImg;
+        }
+
+        private void ScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(backgroundImg, ClientRectangle);
+        }
+    }
+}
